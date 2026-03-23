2336a450a09e3440a20e6f6982d414dc1d61729f
Chan Samnang <samnanggchan@gmail.com>
2026-03-23 14:54:13 +0700

    feat: fix isssues in emailService

--- src/utils/emailService.js
+++ src/utils/emailService.js
@@ -8,9 +8,9 @@ dns.setDefaultResultOrder('ipv4first');
 
 function createTransporter() {
   return nodemailer.createTransport({
-    host:   process.env.EMAIL_HOST || 'smtp.gmail.com',
-    port:   parseInt(process.env.EMAIL_PORT || '587'),
-    secure: process.env.EMAIL_PORT === '587' || process.env.EMAIL_SECURE ==='false',
+    host: process.env.EMAIL_HOST || 'smtp.gmail.com',
+    port: 587,
+    secure: false, 
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
